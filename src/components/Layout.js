83177a805118e96a0bd33e403ca56d748b3fdf63
Ball Panupan <Ball@reverseads.com>
2022-03-04 14:03:12 +0700

    layout children

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
 
-export default function Layout() {
+export default function Layout({ children }) {
   return (
     <div className='layout'>
         <Navbar/>
         <div className='content'>
-        {/* content for each page */}
+            { children }
         </div>
         <footer>
             <p>Cpoyright 2021 Web Warrior</p>
