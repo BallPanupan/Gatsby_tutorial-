97dd926e71a5ae47cb35526f946729940960f038
Ball Panupan <Ball@reverseads.com>
2022-03-04 14:33:20 +0700

    update layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 export default function about() {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>lorem ipsum dolor sot amet </p>
-        </div>
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>lorem ipsum dolor sot amet </p>
+            </div>
+        </Layout>
     )
 }
