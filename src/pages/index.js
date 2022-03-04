de46a50e9281b0a75c4d683483e331257eebd12a
Ball Panupan <Ball@reverseads.com>
2022-03-04 15:21:48 +0700

    update : layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,12 @@ import Layout from "../components/Layout"
 
 export default function Home() {
   return (
-    <section>
-      <Layout>
-        <div>  
-          <h2>Design</h2>
-          <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer based in Manchester.</p>
-        </div>
-      </Layout>
-    </section>
+    <Layout>
+      <div>  
+        <h2>Design</h2>
+        <h3>Develop & Deploy</h3>
+        <p>UX designer & web developer based in Manchester.</p>
+      </div>
+    </Layout>
   )
 }
