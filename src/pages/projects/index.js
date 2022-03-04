3b6d1ab511088ab1f9b184246f2ba63e818b7c9f
Ball Panupan <Ball@reverseads.com>
2022-03-04 10:52:07 +0700

    Create index of Project folder

---
+++ src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div>
+        <h2>Portfolio</h2>
+        <h3>project & Websites i've Created</h3>
+    </div>
+  )
+}
