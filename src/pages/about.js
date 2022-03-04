7d31c06a7480c0bd8fad1a3cb2093e8df712ebae
Ball Panupan <Ball@reverseads.com>
2022-03-04 10:46:01 +0700

    create about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function about() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>lorem ipsum dolor sot amet </p>
+        </div>
+    )
+}
