3aa6c8da770c0544711befd2e5f0ba86ea4da501
Ball Panupan <Ball@reverseads.com>
2022-03-06 15:46:07 +0700

    Hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,7 @@ import { graphql, Link } from 'gatsby'
 import Layout from "../components/Layout"
 import styles from '../styles/home.module.css'
 
-export default function Home({ data }) {
-  console.log(data)
-  const { title, description } = data.site.siteMetadata
-
+export default function Home() {
   return (
     <Layout>
       <section className={styles.header}>
@@ -17,19 +14,7 @@ export default function Home({ data }) {
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
         <img src="/banner.jpg" alt="site banner" style={{ maxWidth: '100%' }}/>
-        <p>{ title } - { description }</p>
       </section>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query SiteInfo {
-    site {
-      siteMetadata {
-        description
-        title
-      }
-    }
-  }
-`
