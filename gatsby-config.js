f844eb7e6d1ec6d1bc977743a20a7ec6dd675ab9
Ball Panupan <Ball@reverseads.com>
2022-03-07 09:19:37 +0700

    add new file for content

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,17 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `notes`,
